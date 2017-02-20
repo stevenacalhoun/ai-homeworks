071fa708f2dd401126a6941194ac4e4e76f3937e
stevenacalhoun <stevenacalhoun@gmail.com>
2017-02-20 11:29:03 -0500

    Added my files, fixed spacing

--- homework5/MyMinion.py
+++ homework5/MyMinion.py
@@ -17,7 +17,7 @@
 '''
 
 import sys, pygame, math, numpy, random, time, copy
-from pygame.locals import * 
+from pygame.locals import *
 
 from constants import *
 from utils import *
@@ -25,18 +25,18 @@ from core import *
 from moba import *
 
 class MyMinion(Minion):
-	
-	def __init__(self, position, orientation, world, image = NPC, speed = SPEED, viewangle = 360, hitpoints = HITPOINTS, firerate = FIRERATE, bulletclass = SmallBullet):
-		Minion.__init__(self, position, orientation, world, image, speed, viewangle, hitpoints, firerate, bulletclass)
-		self.states = [Idle]
-		### Add your states to self.states (but don't remove Idle)
-		### YOUR CODE GOES BELOW HERE ###
 
-		### YOUR CODE GOES ABOVE HERE ###
+  def __init__(self, position, orientation, world, image = NPC, speed = SPEED, viewangle = 360, hitpoints = HITPOINTS, firerate = FIRERATE, bulletclass = SmallBullet):
+    Minion.__init__(self, position, orientation, world, image, speed, viewangle, hitpoints, firerate, bulletclass)
+    self.states = [Idle]
+    ### Add your states to self.states (but don't remove Idle)
+    ### YOUR CODE GOES BELOW HERE ###
 
-	def start(self):
-		Minion.start(self)
-		self.changeState(Idle)
+    ### YOUR CODE GOES ABOVE HERE ###
+
+  def start(self):
+    Minion.start(self)
+    self.changeState(Idle)
 
 
 
@@ -48,18 +48,18 @@ class MyMinion(Minion):
 ### This is the default state of MyMinion. The main purpose of the Idle state is to figure out what state to change to and do that immediately.
 
 class Idle(State):
-	
-	def enter(self, oldstate):
-		State.enter(self, oldstate)
-		# stop moving
-		self.agent.stopMoving()
-	
-	def execute(self, delta = 0):
-		State.execute(self, delta)
-		### YOUR CODE GOES BELOW HERE ###
 
-		### YOUR CODE GOES ABOVE HERE ###
-		return None
+  def enter(self, oldstate):
+    State.enter(self, oldstate)
+    # stop moving
+    self.agent.stopMoving()
+
+  def execute(self, delta = 0):
+    State.execute(self, delta)
+    ### YOUR CODE GOES BELOW HERE ###
+
+    ### YOUR CODE GOES ABOVE HERE ###
+    return None
 
 ##############################
 ### Taunt
@@ -69,14 +69,13 @@ class Idle(State):
 
 class Taunt(State):
 
-	def parseArgs(self, args):
-		self.victim = args[0]
+  def parseArgs(self, args):
+    self.victim = args[0]
 
-	def execute(self, delta = 0):
-		if self.victim is not None:
-			print "Hey " + str(self.victim) + ", I don't like you!"
-		self.agent.changeState(Idle)
+  def execute(self, delta = 0):
+    if self.victim is not None:
+      print "Hey " + str(self.victim) + ", I don't like you!"
+    self.agent.changeState(Idle)
 
 ##############################
 ### YOUR STATES GO HERE:
-
